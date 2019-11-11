33e8e2f8c17108ce68e00dd20d1f376bac7be840
thelamer <ryankuba@gmail.com>
2019-11-11 14:55:40 -0800

    adding kubunut bionic

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://torrent.ubuntu.com/xubuntu/releases/bionic/release/desktop/xubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
+URL="https://torrent.ubuntu.com/kubuntu/releases/bionic/release/desktop/kubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
