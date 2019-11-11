c629718aab910e57f775154900ccaef4406f2edb
thelamer <ryankuba@gmail.com>
2019-11-11 15:21:25 -0800

    adding ubuntu mate bionic

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://torrent.ubuntu.com/ubuntu-budgie/releases/bionic/release/desktop/ubuntu-budgie-REPLACE_VERSION-desktop-amd64.iso.torrent"
+URL="https://torrent.ubuntu.com/ubuntu-mate/releases/bionic/release/desktop/ubuntu-mate-REPLACE_VERSION-desktop-amd64.iso.torrent"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
