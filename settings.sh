fbf44fffbefd563fadb7bd956c448655f1bf6a5b
thelamer <ryankuba@gmail.com>
2019-11-11 15:16:14 -0800

    adding budgie bionic

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://torrent.ubuntu.com/lubuntu/releases/bionic/release/desktop/lubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
+URL="https://torrent.ubuntu.com/ubuntu-budgie/releases/bionic/release/desktop/ubuntu-budgie-REPLACE_VERSION-desktop-amd64.iso.torrent"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
