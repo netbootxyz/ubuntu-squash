d9bd73c3699d84adf914fbc1ecffa9bfd5675f72
thelamer <ryankuba@gmail.com>
2019-11-11 00:59:44 -0800

    adding xubuntu

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="http://releases.ubuntu.com/18.04/ubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
+URL="https://torrent.ubuntu.com/xubuntu/releases/bionic/release/desktop/xubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
