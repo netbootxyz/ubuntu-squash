2f9cd5bef927d0c6d06da41b716399594718481a
thelamer <ryankuba@gmail.com>
2019-11-11 15:00:50 -0800

    adding lubuntu

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://torrent.ubuntu.com/kubuntu/releases/bionic/release/desktop/kubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
+URL="https://torrent.ubuntu.com/lubuntu/releases/bionic/release/desktop/lubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
