b243fefb6dc575bf0049ae4c2ed6b5e39f8cd84d
thelamer <ryankuba@gmail.com>
2019-11-14 00:46:17 -0800

    adding eoan kubuntu squash

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://torrent.ubuntu.com/kubuntu/releases/bionic/release/desktop/kubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
+URL="https://torrent.ubuntu.com/kubuntu/releases/eoan/release/desktop/kubuntu-19.10-desktop-amd64.iso.torrent"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
