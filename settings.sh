48069fc4e35583a607d5c9c3ca66a532bcffe7ab
thelamer <ryankuba@gmail.com>
2019-11-14 01:01:08 -0800

    adding ubuntu-mate eoan branch

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://torrent.ubuntu.com/ubuntu-mate/releases/bionic/release/desktop/ubuntu-mate-REPLACE_VERSION-desktop-amd64.iso.torrent"
+URL="https://torrent.ubuntu.com/ubuntu-mate/releases/eoan/release/desktop/ubuntu-mate-19.10-desktop-amd64.iso.torrent"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
