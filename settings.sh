bda09025e8a3b4edcbb0eb6adbbe1e11bee7dd24
thelamer <ryankuba@gmail.com>
2019-11-14 01:09:10 -0800

    adding ubuntu-budgie eoan branch

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://torrent.ubuntu.com/ubuntu-budgie/releases/bionic/release/desktop/ubuntu-budgie-REPLACE_VERSION-desktop-amd64.iso.torrent"
+URL="https://torrent.ubuntu.com/ubuntu-budgie/releases/eoan/release/desktop/ubuntu-budgie-19.10-desktop-amd64.iso.torrent"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
