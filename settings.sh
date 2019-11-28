8bd33b192b95f02bd7c09574aaea4458845f5e5e
thelamer <ryankuba@gmail.com>
2019-11-28 15:36:02 -0800

    adding elementary os in this space due to it being completely tied to ubuntu

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="http://releases.ubuntu.com/18.04/ubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
+URL="magnet:?xt=urn:btih:f5b31b1bd67bf65fe97be298ec7c473cb2e3e201&dn=elementaryos-5.0-stable.20181016.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&ws=http://sfo1.dl.elementary.io/download/MTU3NDk4MzM4Mg==/elementaryos-5.0-stable.20181016.iso"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
