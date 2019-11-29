b1e9a9cc8789f6ddac12b72babb97e4460feed5e
thelamer <ryankuba@gmail.com>
2019-11-29 14:29:12 -0800

    adding popOS 18.04

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://pop-iso.sfo2.cdn.digitaloceanspaces.com/19.10/amd64/intel/REPLACE_VERSION/pop-os_19.10_amd64_intel_REPLACE_VERSION.iso"
+URL="https://pop-iso.sfo2.cdn.digitaloceanspaces.com/18.04/amd64/intel/REPLACE_VERSION/pop-os_18.04_amd64_intel_REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
-casper_pop-os_19.10_*/filesystem.squashfs|filesystem.squashfs"
+casper_pop-os_18.04_*/filesystem.squashfs|filesystem.squashfs"
