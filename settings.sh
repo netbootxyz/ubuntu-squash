3bce362b7068b1c65fe5f0433476d98b0dd11770
thelamer <ryankuba@gmail.com>
2019-12-01 19:39:14 -0800

    adding peppermint 10 assets

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="http://releases.ubuntu.com/18.04/ubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
-TYPE=torrent
+URL="https://github.com/peppermintos/Peppermint-10/releases/download/10_20190514/Peppermint-10-20190514-amd64.iso"
+TYPE=file
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
