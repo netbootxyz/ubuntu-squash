539182bf38ab6ef00db6af722e1b325db1774181
thelamer <ryankuba@gmail.com>
2019-12-02 01:19:09 -0800

    adding voyager bionic variant

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="http://releases.ubuntu.com/18.04/ubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
+URL="https://downloads.sourceforge.net/project/voyagerlive/Voyager-18.04.2-amd64.iso"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
