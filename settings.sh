24926a26d15ec874a0029be2197d654a12c2fa05
thelamer <ryankuba@gmail.com>
2019-12-01 18:44:35 -0800

    adding zorin xfce flavor

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://downloads.sourceforge.net/project/zorin-os/15/Zorin-OS-15-Core-64-bit-r1.iso"
+URL="https://downloads.sourceforge.net/project/zorin-os/15/Zorin-OS-15-Lite-64-bit.iso"
 TYPE=file
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
