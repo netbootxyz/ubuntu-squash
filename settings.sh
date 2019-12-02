5a6a8fabf3cb1dabb945d2dfafd95e2507097772
thelamer <ryankuba@gmail.com>
2019-12-02 01:23:03 -0800

    adding voyager eoan flavor

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="http://releases.ubuntu.com/eoan/ubuntu-19.10-desktop-amd64.iso.torrent"
-TYPE=torrent
+URL="https://downloads.sourceforge.net/project/voyagerlive/Voyager-GE-19.10-amd64.iso"
+TYPE=file
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
