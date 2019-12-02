b44ce5ee6fc18fa59c6a80868d2a381e7980f465
thelamer <ryankuba@gmail.com>
2019-12-01 19:47:13 -0800

    adding backbox 6 assets

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="http://releases.ubuntu.com/18.04/ubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
+URL="https://backbox.lt.mirrors.airvpn.org/backbox-6-desktop-amd64.iso.torrent"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
