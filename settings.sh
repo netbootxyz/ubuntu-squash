14ff2c984ac1b01e9d96b33c9228ecdc4d779981
thelamer <ryankuba@gmail.com>
2019-12-03 13:07:11 -0800

    bump to 5.1

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="magnet:?xt=urn:btih:f5b31b1bd67bf65fe97be298ec7c473cb2e3e201&dn=elementaryos-5.0-stable.20181016.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&ws=http://sfo1.dl.elementary.io/download/MTU3NDk4MzM4Mg==/elementaryos-5.0-stable.20181016.iso"
+URL="magnet:?xt=urn:btih:5b1599770c9f8c796ec8bbe096b8be545510e2c9&dn=elementaryos-5.1-stable.20191202.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&ws=http://sfo1.dl.elementary.io/download/MTU3NTQwNjQ3MQ==/elementaryos-5.1-stable.20191202.iso"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
