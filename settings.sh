f5884501c14842c555b6b1fd92b2af058b0c5784
thelamer <ryankuba@gmail.com>
2019-12-12 17:00:44 -0800

    updating zorin xfce to 15.1

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://downloads.sourceforge.net/project/zorin-os/15/Zorin-OS-15-Lite-64-bit.iso"
+URL="https://downloads.sourceforge.net/project/zorin-os/15/Zorin-OS-15.1-Lite-64-bit.iso"
 TYPE=file
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
