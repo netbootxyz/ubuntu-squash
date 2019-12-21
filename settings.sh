316fd7f733c230dfd3d39c3621d542392c96f297
thelamer <ryankuba@gmail.com>
2019-12-21 13:31:51 -0800

    adding nvidia pop 18.04 flavor

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://pop-iso.sfo2.cdn.digitaloceanspaces.com/18.04/amd64/intel/REPLACE_VERSION/pop-os_18.04_amd64_intel_REPLACE_VERSION.iso"
+URL="https://pop-iso.sfo2.cdn.digitaloceanspaces.com/18.04/amd64/nvidia/REPLACE_VERSION/pop-os_18.04_amd64_nvidia_REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 casper_pop-os_18.04_*/filesystem.squashfs|filesystem.squashfs
