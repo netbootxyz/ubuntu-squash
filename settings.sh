bdc3fe0aea89c9d8e8ce1103db67da88f5df4757
thelamer <ryankuba@gmail.com>
2019-12-21 13:38:47 -0800

    swap over to modifying ISO contents for asset ingestion

--- settings.sh
+++ settings.sh
@@ -1,4 +1,9 @@
 URL="https://pop-iso.sfo2.cdn.digitaloceanspaces.com/19.10/amd64/intel/REPLACE_VERSION/pop-os_19.10_amd64_intel_REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
-casper_pop-os_19.10_*/filesystem.squashfs|filesystem.squashfs"
+casper_pop-os_19.10_*/filesystem.squashfs|filesystem.squashfs
+casper_pop-os_19.10_*/initrd.gz|initrd
+casper_pop-os_19.10_*/vmlinuz.efi|vmlinuz"
+EXTRACT_INITRD="true"
+INITRD_NAME="initrd"
+INITRD_TYPE="lz4"
