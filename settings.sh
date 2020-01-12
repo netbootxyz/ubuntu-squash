ae0906bb1bddd6aa192e73dcd970e6ebac855904
thelamer <ryankuba@gmail.com>
2020-01-12 15:47:20 -0800

    add pureos gnome assets

--- settings.sh
+++ settings.sh
@@ -1,8 +1,8 @@
-URL="https://downloads.sourceforge.net/project/ferenoslinux/Feren%20OS%20x64.iso"
+URL="http://downloads.pureos.net/amber/live/gnome/REPLACE_VERSION/pureos-9.0-gnome-live_$(echo REPLACE_VERSION| sed 's/-//g')-amd64.hybrid.iso"
 TYPE=file
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs
-casper/initrd.lz|initrd
+casper/initrd.img|initrd
 casper/vmlinuz|vmlinuz"
 EXTRACT_INITRD="true"
 INITRD_NAME="initrd"
