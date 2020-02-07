30ea02398d3a33e1f4fb9780183fccd3212bef4c
thelamer <ryankuba@gmail.com>
2020-02-07 07:56:46 -0800

    update download magnet to latest release

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="magnet:?xt=urn:btih:5b1599770c9f8c796ec8bbe096b8be545510e2c9&dn=elementaryos-5.1-stable.20191202.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&ws=http://sfo1.dl.elementary.io/download/MTU3NTQwNjQ3MQ==/elementaryos-5.1-stable.20191202.iso"
+URL="magnet:?xt=urn:btih:50aa76ebbe8396e4afe93c2d8aede35faf5b1a51&dn=elementaryos-5.1-stable.20200204.iso&tr=https%3A%2F%2Fashrise.com%3A443%2Fphoenix%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&ws=http://sfo1.dl.elementary.io/download/MTU4MTA5MDcyNw==/elementaryos-5.1-stable.20200204.iso"
 TYPE=torrent
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs
