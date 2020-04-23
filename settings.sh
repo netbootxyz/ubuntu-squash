e83d5b323be26f3e4218ec6647bbd6dcbef6952f
thelamer <ryankuba@gmail.com>
2020-04-23 11:00:57 -0700

    adding lubuntu focal

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="http://cdimage.ubuntu.com/kubuntu/releases/20.04/release/kubuntu-20.04-desktop-amd64.iso"
+URL="http://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04-desktop-amd64.iso"
 TYPE=file
 CONTENTS="\
 casper/vmlinuz|vmlinuz
