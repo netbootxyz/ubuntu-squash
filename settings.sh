a9f8b4ee6b7d1a546fdd0201be22960c142d4e82
thelamer <ryankuba@gmail.com>
2020-04-23 11:04:35 -0700

    adding budgie focal

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="http://cdimage.ubuntu.com/kubuntu/releases/20.04/release/kubuntu-20.04-desktop-amd64.iso"
+URL="http://cdimage.ubuntu.com/ubuntu-budgie/releases/20.04/release/ubuntu-budgie-20.04-desktop-amd64.iso"
 TYPE=file
 CONTENTS="\
 casper/vmlinuz|vmlinuz
