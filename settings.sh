354472d80642ab960a3ccf42c5da68151f5ebefc
thelamer <ryankuba@gmail.com>
2020-04-29 11:56:30 -0700

    adding base focal flavor

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://downloads.sourceforge.net/project/voyagerlive/Voyager-GE-19.10-amd64.iso"
+URL="https://downloads.sourceforge.net/project/voyagerlive/Voyager-20.04-LTS-amd64.iso"
 TYPE=file
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs
