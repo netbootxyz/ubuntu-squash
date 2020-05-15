8df8aaac992b25d2b87794e01c4692f202ac77e7
thelamer <ryankuba@gmail.com>
2020-05-15 14:50:28 -0700

    adding backbox 7 assets

--- settings.sh
+++ settings.sh
@@ -1,9 +1,9 @@
-URL="https://backbox.lt.mirrors.airvpn.org/backbox-REPLACE_VERSION-desktop-amd64.iso.torrent"
-TYPE=torrent
+URL="https://backbox.lt.mirrors.airvpn.org/backbox-REPLACE_VERSION-desktop-amd64.iso"
+TYPE=file
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs
 casper/initrd|initrd
 casper/vmlinuz|vmlinuz"
 EXTRACT_INITRD="true"
 INITRD_NAME="initrd"
-INITRD_TYPE="gz"
+INITRD_TYPE="lz4"
