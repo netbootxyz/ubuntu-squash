d249deceaf12ac96518202d5fa5e2227572054af
thelamer <ryankuba@gmail.com>
2020-08-16 07:54:19 -0700

    change to file ingestion, update version ingestion

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://torrent.ubuntu.com/ubuntu-budgie/releases/bionic/release/desktop/ubuntu-budgie-REPLACE_VERSION-desktop-amd64.iso.torrent"
-TYPE=torrent
+URL="http://mirror.us.leaseweb.net/ubuntu-cdimage/ubuntu-budgie/releases/18.04/release/ubuntu-budgie-REPLACE_VERSION-desktop-amd64.iso"
+TYPE=file
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
