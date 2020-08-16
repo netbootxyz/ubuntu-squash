f07dca7f555f3d0fe785e5f22334f9c70b85e33a
thelamer <ryankuba@gmail.com>
2020-08-16 07:52:47 -0700

    change to file ingestion, update version ingestion

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://torrent.ubuntu.com/lubuntu/releases/bionic/release/desktop/lubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
-TYPE=torrent
+URL="http://mirror.us.leaseweb.net/ubuntu-cdimage/lubuntu/releases/18.04/release/lubuntu-REPLACE_VERSION-desktop-amd64.iso"
+TYPE=file
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
