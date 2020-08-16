5f0d917bd00c20add32fc5179adb34100e5d215a
thelamer <ryankuba@gmail.com>
2020-08-16 07:51:14 -0700

    change to file ingestion, update version ingestion

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://torrent.ubuntu.com/kubuntu/releases/bionic/release/desktop/kubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
-TYPE=torrent
+URL="http://mirror.us.leaseweb.net/ubuntu-cdimage/kubuntu/releases/18.04/release/kubuntu-REPLACE_VERSION-desktop-amd64.iso"
+TYPE=file
 CONTENTS="\
 casper/filesystem.squashfs|filesystem.squashfs"
