9bac6dd382ba78ddfb95c66e1cba287b2df39a31
Antony Messerli <antony@mes.ser.li>
2020-11-23 12:38:46 -0600

    Add pop 20.10

--- settings.sh
+++ settings.sh
@@ -1,9 +1,9 @@
-URL="https://pop-iso.sfo2.cdn.digitaloceanspaces.com/20.04/amd64/intel/5/pop-os_20.04_amd64_intel_5.iso"
+URL="https://pop-iso.sfo2.cdn.digitaloceanspaces.com/20.10/amd64/intel/2/pop-os_20.10_amd64_intel_2.iso"
 TYPE=file
 CONTENTS="\
-casper_pop-os_20.04_*/filesystem.squashfs|filesystem.squashfs
-casper_pop-os_20.04_*/initrd.gz|initrd
-casper_pop-os_20.04_*/vmlinuz.efi|vmlinuz"
+casper_pop-os_20.10_*/filesystem.squashfs|filesystem.squashfs
+casper_pop-os_20.10_*/initrd.gz|initrd
+casper_pop-os_20.10_*/vmlinuz.efi|vmlinuz"
 EXTRACT_INITRD="true"
 INITRD_NAME="initrd"
 INITRD_TYPE="lz4"
