86af5ae35d24edca03a9e4d9b23a61f8a60b01b8
netboot-ci <netboot-ci@netboot.xyz>
2020-12-19 07:15:17 +0000

    Updating workflow automation

---
+++ version.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+VERSION=$(curl -sL http://mirror.us.leaseweb.net/ubuntu-cdimage/ubuntukylin/releases/18.04/release/SHA256SUMS | awk '{ print $2}' | grep -Po "(\d+\.)+\d+" | sort | tail -1)
+echo "${VERSION}"
