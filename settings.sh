2cb0e38474b70fa874f7efa02bea53ae2a22f122
Antony Messerli <antony@mes.ser.li>
2021-04-25 09:41:14 -0500

    Add lubuntu-21.10

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="http://cdimage.ubuntu.com/lubuntu/releases/focal/release/lubuntu-REPLACE_VERSION-desktop-amd64.iso"
+URL="http://cdimage.ubuntu.com/lubuntu/releases/hirsute/release/lubuntu-REPLACE_VERSION-desktop-amd64.iso"
 TYPE=file
 CONTENTS="\
 casper/vmlinuz|vmlinuz
--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL http://cdimage.ubuntu.com/lubuntu/releases/focal/release/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
+VERSION=$(curl -sL http://cdimage.ubuntu.com/lubuntu/releases/hirsute/release/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
 echo "${VERSION}"
