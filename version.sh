c31dd90cf28411a6d801f081254d4e57a8c2513a
Antony Messerli <antony@mes.ser.li>
2021-09-01 21:44:14 -0500

    Update version.sh

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION="5.4"
+VERSION=$(curl -s https://www.linuxliteos.com/download.php | grep "UEFI/LEGACY" | awk -F '(linux-lite-|-64bit.iso)' '/linux-lite-/ {print $2;exit}')
 echo "${VERSION}"
