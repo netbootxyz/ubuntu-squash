15aa24399b94e9ef33d3d4f6d269b5369f266587
Antony Messerli <antony@mes.ser.li>
2021-09-25 11:50:06 -0500

    Add 21.04 arm64

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://cdimage.ubuntu.com/releases/20.04/release/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
+VERSION=$(curl -sL https://cdimage.ubuntu.com/releases/21.04/release/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
 echo "${VERSION}"
