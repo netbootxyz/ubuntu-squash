694c3ac248d4704336b994828011013833f0f3bf
Antony Messerli <antony@mes.ser.li>
2021-09-28 17:34:41 -0500

    Add 21.10 amd64

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL http://releases.ubuntu.com/hirsute/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
+VERSION=$(curl -sL http://releases.ubuntu.com/impish/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
 echo "${VERSION}"
