22689ddec3cba532ab6b549f4423aaea84d14499
Antony Messerli <antony@mes.ser.li>
2021-09-29 17:37:31 -0500

    Update job

--- settings.sh
+++ settings.sh
@@ -3,7 +3,10 @@ TYPE=file
 CONTENTS="\
 casper/vmlinuz|vmlinuz
 casper/initrd|initrd
-casper/filesystem.squashfs|filesystem.squashfs"
+casper/ubuntu-server-minimal.squashfs|ubuntu-server-minimal.squashfs
+casper/ubuntu-server-minimal.ubuntu-server.installer.generic.squashfs|ubuntu-server-minimal.ubuntu-server.installer.generic.squashfs
+casper/ubuntu-server-minimal.ubuntu-server.installer.squashfs|ubuntu-server-minimal.ubuntu-server.installer.squashfs
+casper/ubuntu-server-minimal.ubuntu-server.squashfs|ubuntu-server-minimal.ubuntu-server.squashfs"
 EXTRACT_INITRD="false"
 INITRD_NAME="initrd"
 INITRD_TYPE="lz4"
