8741eec221d6e7300873c809ca3787b35cd04cb2
Antony Messerli <antony@mes.ser.li>
2021-10-15 10:26:11 -0500

    Drop beta tag

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="http://releases.ubuntu.com/impish/ubuntu-REPLACE_VERSION-beta-live-server-amd64.iso"
+URL="http://releases.ubuntu.com/impish/ubuntu-REPLACE_VERSION-live-server-amd64.iso"
 TYPE=file
 CONTENTS="\
 casper/vmlinuz|vmlinuz
