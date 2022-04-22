595a3acb2f1f538414c6d4048e0f41ab04f3d6bc
Antony Messerli <antony@mes.ser.li>
2022-04-21 22:42:38 -0500

    Add 22.04

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL http://cdimage.ubuntu.com/ubuntu-mate/releases/impish/release/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
+VERSION=$(curl -sL http://cdimage.ubuntu.com/ubuntu-mate/releases/jammy/release/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
 echo "${VERSION}"
