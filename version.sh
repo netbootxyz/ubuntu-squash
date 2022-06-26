5f251ca828ccf81b8c3d021e055a69c4f4900785
Antony Messerli <antony@mes.ser.li>
2022-06-26 16:57:38 -0500

    Update url

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://clonezilla.org/downloads/alternative-testing/data/CHECKSUMS.TXT | awk -F '(clonezilla-live-|-amd64.iso)' '/clonezilla-live-/ {print $2;exit}')
+VERSION=$(curl -sL https://clonezilla.org/downloads/alternative-testing/data_2/CHECKSUMS.TXT | awk -F '(clonezilla-live-|-amd64.iso)' '/clonezilla-live-/ {print $2;exit}')
 echo "${VERSION}"
