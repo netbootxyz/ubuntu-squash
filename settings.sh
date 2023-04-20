89ddd9b061908112ce67e68b103403523845a6c3
Antony Messerli <antony@mes.ser.li>
2023-04-20 18:59:46 -0500

    Bump for 23.04

--- settings.sh
+++ settings.sh
@@ -1,12 +1,8 @@
-URL="http://releases.ubuntu.com/kinetic/ubuntu-REPLACE_VERSION-live-server-amd64.iso"
+URL="http://releases.ubuntu.com/lunar/ubuntu-REPLACE_VERSION-live-server-amd64.iso"
 TYPE=file
 CONTENTS="\
 casper/vmlinuz|vmlinuz
-casper/initrd|initrd
-casper/ubuntu-server-minimal.squashfs|ubuntu-server-minimal.squashfs
-casper/ubuntu-server-minimal.ubuntu-server.installer.generic.squashfs|ubuntu-server-minimal.ubuntu-server.installer.generic.squashfs
-casper/ubuntu-server-minimal.ubuntu-server.installer.squashfs|ubuntu-server-minimal.ubuntu-server.installer.squashfs
-casper/ubuntu-server-minimal.ubuntu-server.squashfs|ubuntu-server-minimal.ubuntu-server.squashfs"
+casper/initrd|initrd"
 EXTRACT_INITRD="false"
 INITRD_NAME="initrd"
 INITRD_TYPE="lz4"
--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL http://releases.ubuntu.com/kinetic/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
+VERSION=$(curl -sL http://releases.ubuntu.com/lunar/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
 echo "${VERSION}"
