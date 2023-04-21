b494ec82fd6f5728bc9148c98d3413354e50c1f7
Antony Messerli <antony@mes.ser.li>
2023-04-20 19:03:43 -0500

    Fix ver

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://cdimage.ubuntu.com/releases/22.04/release/ubuntu-REPLACE_VERSION-live-server-arm64.iso"
+URL="https://cdimage.ubuntu.com/releases/23.04/release/ubuntu-REPLACE_VERSION-live-server-arm64.iso"
 TYPE=file
 CONTENTS="\
 casper/vmlinuz|vmlinuz
