4dd4b19598c80ac6e16a84cafe0836e3375ab372
Antony Messerli <antony@mes.ser.li>
2023-04-24 23:44:02 -0500

    Bump to 23.04

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://releases.ubuntu.com/kinetic/ubuntu-REPLACE_VERSION-desktop-amd64.iso"
+URL="https://releases.ubuntu.com/lunar/ubuntu-REPLACE_VERSION-desktop-amd64.iso"
 TYPE=file
 CONTENTS="\
 casper/vmlinuz|vmlinuz
