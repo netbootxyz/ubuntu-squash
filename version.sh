8ae82ca56d089c2c52b264a79cd88ef09f33d70d
Antony Messerli <antony@mes.ser.li>
2023-04-24 23:41:58 -0500

    Bump to 23.04

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL http://cdimage.ubuntu.com/ubuntu-budgie/releases/kinetic/release/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
+VERSION=$(curl -sL http://cdimage.ubuntu.com/ubuntu-budgie/releases/lunar/release/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
 echo "${VERSION}"
