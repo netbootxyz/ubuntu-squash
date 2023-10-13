27482c204ecf3349a116b7cf0d91597c646352be
Antony Messerli <antony@mes.ser.li>
2023-10-13 16:22:10 -0500

    Add Gnome 23.10

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL http://releases.ubuntu.com/lunar/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
+VERSION=$(curl -sL http://releases.ubuntu.com/mantic/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
 echo "${VERSION}"
