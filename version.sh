1b47aafc66d39682cb4f4cf9128c25cae4bc9cd0
Antony Messerli <antony@mes.ser.li>
2023-11-29 00:54:11 -0600

    Fix Zorin

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://zorin.com/os/download/ | grep Core | head -n 1 | awk -F' ' {'print $6'} | awk -F'<' {'print $1'})
+VERSION=$(curl -sL https://zorin.com/os/download/ | grep Core | grep -v subscribe | head -n 1 | awk -F' ' {'print $6'} | awk -F'<' {'print $1'})
 echo "${VERSION}"
