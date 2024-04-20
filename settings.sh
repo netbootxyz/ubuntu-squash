295657f30fbaaae7751e953023269c3ec993febc
Antony Messerli <antony@mes.ser.li>
2024-04-20 15:43:55 -0500

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -2,11 +2,7 @@ URL="http://releases.ubuntu.com/jammy/ubuntu-REPLACE_VERSION-live-server-amd64.i
 TYPE=file
 CONTENTS="\
 casper/vmlinuz|vmlinuz
-casper/initrd|initrd
-casper/ubuntu-server-minimal.squashfs|ubuntu-server-minimal.squashfs
-casper/ubuntu-server-minimal.ubuntu-server.installer.generic.squashfs|ubuntu-server-minimal.ubuntu-server.installer.generic.squashfs
-casper/ubuntu-server-minimal.ubuntu-server.installer.squashfs|ubuntu-server-minimal.ubuntu-server.installer.squashfs
-casper/ubuntu-server-minimal.ubuntu-server.squashfs|ubuntu-server-minimal.ubuntu-server.squashfs"
+casper/initrd|initrd"
 EXTRACT_INITRD="false"
 INITRD_NAME="initrd"
 INITRD_TYPE="lz4"
