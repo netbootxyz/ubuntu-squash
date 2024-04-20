32a0e326806e52bd805b04bbfc0e0c5f4c250e6c
Antony Messerli <antony@mes.ser.li>
2024-04-20 15:44:10 -0500

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -2,11 +2,7 @@ URL="https://cdimage.ubuntu.com/releases/22.04/release/ubuntu-REPLACE_VERSION-li
 TYPE=file
 CONTENTS="\
 casper/vmlinuz|vmlinuz
-casper/initrd|initrd
-casper/ubuntu-server-minimal.squashfs|ubuntu-server-minimal.squashfs
-casper/ubuntu-server-minimal.ubuntu-server.installer.generic.squashfs|ubuntu-server-minimal.ubuntu-server.installer.generic.squashfs
-casper/ubuntu-server-minimal.ubuntu-server.installer.squashfs|ubuntu-server-minimal.ubuntu-server.installer.squashfs
-casper/ubuntu-server-minimal.ubuntu-server.squashfs|ubuntu-server-minimal.ubuntu-server.squashfs"
+casper/initrd|initrd"
 EXTRACT_INITRD="false"
 INITRD_NAME="initrd"
 INITRD_TYPE="lz4"
