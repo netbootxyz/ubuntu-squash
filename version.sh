8b679ff77f0df3679bfb8ceb19f74b98dff60078
Antony Messerli <antony@mes.ser.li>
2024-04-20 15:23:11 -0500

    Hard set version

--- version.sh
+++ version.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://cdimage.ubuntu.com/releases/23.10/release/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
+VERSION=23.10
+#VERSION=$(curl -sL https://cdimage.ubuntu.com/releases/23.10/release/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
 echo "${VERSION}"
