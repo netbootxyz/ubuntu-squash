5d65e4e972b2e957ef7172365fb9b142bd7fe786
Antony Messerli <antony@mes.ser.li>
2026-03-15 13:37:44 -0500

    Add HWE kernel variant for Ubuntu 24.04 amd64
    Extract HWE kernel/initrd (casper/hwe-vmlinuz, casper/hwe-initrd)
    instead of generic variants from the Ubuntu 24.04 live-server ISO.
    The HWE kernel provides newer hardware support needed for recent
    server platforms.

--- settings.sh
+++ settings.sh
@@ -1,8 +1,8 @@
 URL="http://releases.ubuntu.com/noble/ubuntu-REPLACE_VERSION-live-server-amd64.iso"
 TYPE=file
 CONTENTS="\
-casper/vmlinuz|vmlinuz
-casper/initrd|initrd"
+casper/hwe-vmlinuz|vmlinuz
+casper/hwe-initrd|initrd"
 EXTRACT_INITRD="false"
 INITRD_NAME="initrd"
 INITRD_TYPE="lz4"
